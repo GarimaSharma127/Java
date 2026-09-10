969e52d8c772a7e53721f6738619845c2f1fc2f9
Garima Sharma <garimasharma.gs126@gmail.com>
2026-09-10 17:29:23 +0530

    added files

---
+++ Basics/RightAngTriangle.java
@@ -0,0 +1,9 @@
+public class RightAngTriangle {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
