c003e03c1a2b94f47277cddb88e83a7cea98a0ca
Garima Sharma <garimasharma.gs126@gmail.com>
2026-09-10 19:16:16 +0530

    Learnt mathematical if else conditioning

---
+++ Basics/EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter one number:");
+        int a=sc.nextInt();
+        if(a%2==0){
+            System.out.println(a+" is an even number");
+        }
+        else{
+            System.out.println(a+" is an odd number");
+        }
+        sc.close();
+    }
+    
+}
