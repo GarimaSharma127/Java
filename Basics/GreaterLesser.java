d01bed7a0daede870fd2059bca6746f0f1b499f3
Garima Sharma <garimasharma.gs126@gmail.com>
2026-09-11 16:37:25 +0530

    Progress

---
+++ Basics/GreaterLesser.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class GreaterLesser {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any two numbers:");
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println(a+" is greater than b");
+        }
+        else if(b>a){
+            System.out.println(b+" is greater than a");
+        }
+        else{
+            System.out.println("Both numbers are equal");
+        }
+        sc.close();
+    }
+    
+}
